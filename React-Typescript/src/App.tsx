fd76c3d3edb174036645b49322ce3bfa89d765da
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-04-08 23:46:22 +0800

    added input field and handle text input

--- React-Typescript/src/App.tsx
+++ React-Typescript/src/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+
+  // prevent reloading the page when the form is submitted
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
+      <header className="App-header">Kanban</header>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
-}
+};
 
 export default App;

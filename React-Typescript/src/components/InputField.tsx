498db6e703450c120da3bd880ce3c96abb93ff1e
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-04-09 22:43:57 +0800

    added useRef for added functionality

--- React-Typescript/src/components/InputField.tsx
+++ React-Typescript/src/components/InputField.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import "./InputField.css";
+import React, { useRef } from "react";
+import "./styles.css";
 
+// uses interface to define the type of todo, setTodo, and handleAdd
 interface InputFieldProps {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
@@ -8,11 +9,21 @@ interface InputFieldProps {
 }
 
 const InputField = ({ todo, setTodo, handleAdd }: InputFieldProps) => {
-  console.log(todo);
+  // to remove the focus from the input field after submitting the form
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="input" onSubmit={handleAdd}>
+    <form
+      className="input"
+      // inputRef in onSubmit
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input
+        // inputRef in input tag to work
+        ref={inputRef}
         type="input"
         placeholder="Enter a task"
         className="Input__box"

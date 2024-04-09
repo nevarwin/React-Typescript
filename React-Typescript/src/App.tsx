362df128993a9076d17df044a1429b038066dd43
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-04-09 22:43:17 +0800

    added functionality to the input field

--- React-Typescript/src/App.tsx
+++ React-Typescript/src/App.tsx
@@ -1,19 +1,39 @@
 import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todos } from "./Todo";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todos[]>([]);
 
-  // prevent reloading the page when the form is submitted
+  // added React.FormEvent for the type of e
   const handleAdd = (e: React.FormEvent) => {
+    // prevent reloading the page when the form is submitted
     e.preventDefault();
+
+    if (todo) {
+      setTodos([
+        ...todos,
+        {
+          id: todos.length + 1,
+          title: todo,
+          isDone: false,
+        },
+      ]);
+      setTodo("");
+    }
   };
+
+  console.log(todos);
 
   return (
     <div className="App">
       <header className="App-header">Kanban</header>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {todos.map((t) => (
+        <li>{t.title}</li>
+      ))}
     </div>
   );
 };

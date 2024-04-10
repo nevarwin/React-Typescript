db8e6c9177b7d9b90590b5585e1796c3dcb9d80d
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-04-10 20:08:38 +0800

    imported the single  todo and respond to the completed todo function

--- React-Typescript/src/components/TodoList.tsx
+++ React-Typescript/src/components/TodoList.tsx
@@ -6,19 +6,38 @@ import SingleTodo from "./SingleTodo";
 interface TodoListProps {
   todos: Todos[];
   setTodos: React.Dispatch<React.SetStateAction<Todos[]>>;
+  completedTodos: Todos[];
+  setCompletedTodos: React.Dispatch<React.SetStateAction<Todos[]>>;
 }
 
 const TodoList = ({ todos, setTodos }: TodoListProps) => {
   return (
-    <div className="todos">
-      {todos.map((todo) => (
-        <SingleTodo
-          key={todo.id}
-          todo={todo}
-          todos={todos}
-          setTodos={setTodos}
-        />
-      ))}
+    <div className="container">
+      {/* Active tasks div */}
+      <div className="todos">
+        <span className="todos_heading">Active Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            key={todo.id}
+            todo={todo}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+
+      {/* Completed tasks div */}
+      <div className="todos  remove">
+        <span className="todos_heading">Completed Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            key={todo.id}
+            todo={todo}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
     </div>
   );
 };

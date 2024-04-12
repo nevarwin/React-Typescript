a127e885cecbd05fffd3e6afa19c9dbb3fd8f691
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-04-12 16:48:36 +0800

    make the single todo list draggable

--- React-Typescript/src/components/SingleTodo.tsx
+++ React-Typescript/src/components/SingleTodo.tsx
@@ -3,6 +3,7 @@ import "./styles.css";
 import { Todos } from "../Todo";
 import { MdEdit, MdDelete } from "react-icons/md";
 import { FaCheck } from "react-icons/fa";
+import { Draggable } from "react-beautiful-dnd";
 
 // uses interface
 type SingleTodoProps = {
@@ -10,9 +11,10 @@ type SingleTodoProps = {
   todos: Todos[];
   todo: Todos;
   setTodos: React.Dispatch<React.SetStateAction<Todos[]>>;
+  index: number;
 };
 
-const SingleTodo = ({ todos, todo, setTodos }: SingleTodoProps) => {
+const SingleTodo = ({ todos, todo, setTodos, index }: SingleTodoProps) => {
   const [edit, setEdit] = useState<boolean>(false);
   const [editValue, setEditValue] = useState<string>(todo.title);
   const inputRef = useRef<HTMLInputElement>(null);
@@ -41,41 +43,51 @@ const SingleTodo = ({ todos, todo, setTodos }: SingleTodoProps) => {
   };
 
   return (
-    <form className="todos_single" onSubmit={(e) => handleEdit(e, todo.id)}>
-      {edit ? (
-        <input
-          ref={inputRef}
-          className="todos_single_text inputEdit"
-          value={editValue}
-          onChange={(e) => setEditValue(e.target.value)}
-          onBlur={(e) => handleEdit(e, todo.id)}
-          autoFocus
-        />
-      ) : todo.isDone ? (
-        <s className="todos_single_text">{todo.title}</s>
-      ) : (
-        <span className="todos_single_text">{todo.title}</span>
-      )}
-
-      <div>
-        <span
-          className="icon"
-          onClick={() => {
-            if (!edit && !todo.isDone) {
-              setEdit(!edit);
-            }
-          }}
+    <Draggable draggableId={todo.id.toString()} index={index}>
+      {(provided) => (
+        <form
+          className="todos_single"
+          onSubmit={(e) => handleEdit(e, todo.id)}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
         >
-          <MdEdit />
-        </span>
-        <span className="icon" onClick={() => handleDelete(todo.id)}>
-          <MdDelete />
-        </span>
-        <span className="icon" onClick={() => handleDone(todo.id)}>
-          <FaCheck />
-        </span>
-      </div>
-    </form>
+          {edit ? (
+            <input
+              ref={inputRef}
+              className="todos_single_text inputEdit"
+              value={editValue}
+              onChange={(e) => setEditValue(e.target.value)}
+              onBlur={(e) => handleEdit(e, todo.id)}
+              autoFocus
+            />
+          ) : todo.isDone ? (
+            <s className="todos_single_text">{todo.title}</s>
+          ) : (
+            <span className="todos_single_text">{todo.title}</span>
+          )}
+
+          <div>
+            <span
+              className="icon"
+              onClick={() => {
+                if (!edit && !todo.isDone) {
+                  setEdit(!edit);
+                }
+              }}
+            >
+              <MdEdit />
+            </span>
+            <span className="icon" onClick={() => handleDelete(todo.id)}>
+              <MdDelete />
+            </span>
+            <span className="icon" onClick={() => handleDone(todo.id)}>
+              <FaCheck />
+            </span>
+          </div>
+        </form>
+      )}
+    </Draggable>
   );
 };
 

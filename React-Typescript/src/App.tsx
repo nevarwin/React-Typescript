f5903838e144f321a2aa9dc265be9aa98e82e5fc
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-04-12 16:48:11 +0800

    added dragdrop functionality

--- React-Typescript/src/App.tsx
+++ React-Typescript/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import InputField from "./components/InputField";
 import TodoList from "./components/TodoList";
 import { Todos } from "./Todo";
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
 const App: React.FC = () => {
   // state for the inputed todo (string)
@@ -32,22 +32,61 @@ const App: React.FC = () => {
     }
   };
 
-  console.log(todos);
+  const onDragEnd = (results: DropResult) => {
+    console.log(results);
+
+    const { destination, source } = results;
+
+    if (!destination) {
+      return;
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    let add,
+      active = todos,
+      completed = completedTodos;
+
+    console.log(source.droppableId);
+
+    if (source.droppableId === "todoActive") {
+      add = active[source.index];
+      active.splice(source.index, 1);
+    } else {
+      add = completed[source.index];
+      completed.splice(source.index, 1);
+    }
+
+    if (destination.droppableId === "todoCompleted") {
+      active.splice(destination.index, 0, add);
+    } else {
+      completed.splice(destination.index, 0, add);
+    }
+
+    setCompletedTodos(completed);
+    setTodos(active);
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">Kanban</header>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      <TodoList
-        todos={todos}
-        setTodos={setTodos}
-        completedTodos={completedTodos}
-        setCompletedTodos={setCompletedTodos}
-      />
-      {/* {todos.map((t) => (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App">
+        <header className="App-header">Kanban</header>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          completedTodos={completedTodos}
+          setCompletedTodos={setCompletedTodos}
+        />
+        {/* {todos.map((t) => (
         <li>{t.title}</li>
       ))} */}
-    </div>
+      </div>
+    </DragDropContext>
   );
 };
 
